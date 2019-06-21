2da00c2b540739fa9a954d595858421e64cfee55
Durgesh Chaporkar <durgesh.chaporkar@ril.com>
2019-06-21 16:14:48 +0530

    Updated with the first version

--- src/test/java/com/ee/orderservice/OrderServiceTest.java
+++ src/test/java/com/ee/orderservice/OrderServiceTest.java
@@ -1,11 +1,11 @@
 package com.ee.orderservice;
 
-import au.com.dius.pact.consumer.Pact;
-import au.com.dius.pact.consumer.PactProviderRuleMk2;
-import au.com.dius.pact.consumer.PactVerification;
-import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
-import au.com.dius.pact.model.RequestResponsePact;
-import com.ee.orderservice.OrderServiceController.OrderDetails;
+import static io.pactfoundation.consumer.dsl.LambdaDsl.newJsonBody;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+
 import org.jetbrains.annotations.NotNull;
 import org.junit.Before;
 import org.junit.Rule;
@@ -20,73 +20,85 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static io.pactfoundation.consumer.dsl.LambdaDsl.newJsonBody;
-import static org.hamcrest.Matchers.isEmptyOrNullString;
-import static org.hamcrest.Matchers.not;
-import static org.junit.Assert.assertThat;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+import com.ee.orderservice.OrderServiceController.OrderDetails;
+
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRuleMk2;
+import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.RequestResponsePact;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = DEFINED_PORT)
 public class OrderServiceTest {
 
-    public static final String USER_ID = "pareshId";
+	public static final String USER_ID = "pareshId";
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+	@Autowired
+	private TestRestTemplate restTemplate;
 
-    @Rule
-    public PactProviderRuleMk2 mockDepartmentProvider = new PactProviderRuleMk2("user_service", "localhost", 8888, this);
+	@Rule
+	public PactProviderRuleMk2 mockDepartmentProvider = new PactProviderRuleMk2("user_service", "localhost", 8888,
+			this);
 
-    /**
-     * Setup the user service mock server. Method name can be anything but must be annotated with @pact
-     */
-    @Pact(provider = "user_service", consumer = "order_service")
-    public RequestResponsePact createPact(PactDslWithProvider builder) {
-        // TODO - Implement user service mock expectation required for OrderService.
-        // TODO - This will get translated into the contract between order service and user service.
-        return null;
-    }
+	/**
+	 * Setup the user service mock server. Method name can be anything but must be
+	 * annotated with @pact
+	 */
+	@Pact(provider = "user_service", consumer = "order_service")
+	public RequestResponsePact createPact(PactDslWithProvider builder) {
 
+		// TODO - Implement user service mock expectation required for OrderService.
+		// TODO - This will get translated into the contract between order service and
+		// user service.
+		return builder.given("User Paresh exists").uponReceiving("user GET request").path("/user/" + USER_ID)
+				.method("GET").willRespondWith().status(200).body(newJsonBody((a) -> {
+					a.stringValue("name", "paresh");
+					a.stringValue("email", "paresh@ee.com");
+					a.stringValue("address", "pune");
+				}).build()).toPact();
 
-    @Test
-    @PactVerification("user_service")
-    public void springWiringTest() {
-        // Given
-        OrderDetails orderDetails = new OrderDetails("productId", "1", USER_ID);
+	}
 
-        // When
-        OrderResponse orderResponse = restTemplate.postForObject("/order", getHttpRequest(orderDetails), OrderResponse.class);
+	@Test
+	@PactVerification("user_service")
+	public void springWiringTest() {
+		// Given
+		OrderDetails orderDetails = new OrderDetails("productId", "1", USER_ID);
 
-        // then
-        assertThat(orderResponse.getOrderId(), not(isEmptyOrNullString()));
-    }
+		// When
+		OrderResponse orderResponse = restTemplate.postForObject("/order", getHttpRequest(orderDetails),
+				OrderResponse.class);
 
-    static class OrderResponse {
-        private String orderId;
+		// then
+		assertThat(orderResponse.getOrderId(), not(isEmptyOrNullString()));
+	}
 
-        public OrderResponse() {
-            // default constructor.
-        }
+	static class OrderResponse {
+		private String orderId;
 
-        public void setOrderId(String orderId) {
-            this.orderId = orderId;
-        }
+		public OrderResponse() {
+			// default constructor.
+		}
 
-        public String getOrderId() {
-            return orderId;
-        }
-    }
+		public void setOrderId(String orderId) {
+			this.orderId = orderId;
+		}
 
-    @Before
-    public void setup() {
-        restTemplate.getRestTemplate().getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-    }
+		public String getOrderId() {
+			return orderId;
+		}
+	}
 
-    @NotNull
-    private HttpEntity<OrderDetails> getHttpRequest(OrderDetails orderDetails) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        return new HttpEntity<>(orderDetails, httpHeaders);
-    }
+	@Before
+	public void setup() {
+		restTemplate.getRestTemplate().getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+	}
+
+	@NotNull
+	private HttpEntity<OrderDetails> getHttpRequest(OrderDetails orderDetails) {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
+		return new HttpEntity<>(orderDetails, httpHeaders);
+	}
 }
